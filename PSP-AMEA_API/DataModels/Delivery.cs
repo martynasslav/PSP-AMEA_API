dc98b22a0c4362de23ecd35e93d42c8348273a7f
martynas.slav <martynas.slav@gmail.com>
2022-11-17 17:55:37 +0200

    Add Delivery data model.

---
+++ PSP-AMEA_API/DataModels/Delivery.cs
@@ -0,0 +1,11 @@
+﻿namespace PSP_AMEA_API.DataModels
+{
+	public class Delivery
+	{
+		public Guid Id { get; set; }
+		public Guid OrderId { get; set; }
+		public string Address { get; set; } = "Address";
+		public string PostCode { get; set; } = "PostCode";
+		public string Details { get; set; } = "Details";
+	}
+}
