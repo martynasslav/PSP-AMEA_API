b0786ba152ae8b9b5c5c18663ed15112aabc5aae
martynas.slav <martynas.slav@gmail.com>
2022-11-17 17:17:13 +0200

    Add  Review data model.

---
+++ PSP-AMEA_API/DataModels/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace PSP_AMEA_API.DataModels
+{
+	public class Review
+	{
+		public string Description { get; set; } = "Description";
+		public int Rating { get; set; } = 5;
+		public string Photo { get; set; } = "Photo";
+		public Guid ItemId { get; set; }
+		public Guid UserId { get; set; }
+	}
+}
