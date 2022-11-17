0b47f5e30c4eb04ec7a97e2d5a5255282baaf350
martynas.slav <martynas.slav@gmail.com>
2022-11-17 17:53:41 +0200

    Add Payment data model.

---
+++ PSP-AMEA_API/DataModels/Payment.cs
@@ -0,0 +1,11 @@
+﻿namespace PSP_AMEA_API.DataModels
+{
+	public class Payment
+	{
+		public Guid Id { get; set; }
+		public Guid OrderId { get; set; }
+		public string Provider { get; set; } = "Provider";
+		public string Status { get; set; } = "Status";
+
+	}
+}
