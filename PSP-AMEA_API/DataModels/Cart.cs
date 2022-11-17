167cdf4a73b5427aa63d211343e6343f1383ef3a
martynas.slav <martynas.slav@gmail.com>
2022-11-17 18:11:15 +0200

    Add Cart data model.

---
+++ PSP-AMEA_API/DataModels/Cart.cs
@@ -0,0 +1,11 @@
+﻿namespace PSP_AMEA_API.DataModels
+{
+	public class Cart
+	{
+		public Guid ItemId { get; set; }
+		public Guid OrderId { get; set; }
+		public decimal Quantity { get; set; }
+		public decimal Discount { get; set; }
+		public string Description { get; set; } = "Description";
+	}
+}
