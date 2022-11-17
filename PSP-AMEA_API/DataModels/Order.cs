34b31460eba792c66dda002eccb611dbbee699bd
martynas.slav <martynas.slav@gmail.com>
2022-11-17 17:51:34 +0200

    Add Order data model.

---
+++ PSP-AMEA_API/DataModels/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace PSP_AMEA_API.DataModels
+{
+	public class Order
+	{
+		public Guid Id { get; set; }
+		public Guid CustomerId { get; set; }
+		public Guid EmployeeId { get; set; }
+		public Guid TenantId { get; set; }
+		public decimal Total { get; set; }
+		public decimal Tip { get; set;}
+		public string DeliveryType { get; set; } = "DeliveryType";
+		public DateTime Date { get; set; }
+	}
+}
